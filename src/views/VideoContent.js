3dfdda75ff46d355b8ad3c7ec8872a1c45f2288d
Chris Klaiber <chris@khanacademy.org>
2015-07-31 10:39:14 -0700

    Improve SPEEED perception by loading video thumbnail before the video player kicks in

--- src/views/VideoContent.js
+++ src/views/VideoContent.js
@@ -15,12 +15,24 @@ const descriptionBoxStyle = {
   margin: '0 10px 10px 10px',
   padding: '10px'
 };
+const frameStyle = {
+  width: '100%',
+  height: '192px',
+  marginBottom: '8px'
+};
+const frameOverlayStyle = {
+  ...frameStyle,
+  position: 'absolute',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover'
+};
 
 @connect((state, props) => ({
   videoData: state.video[props.params.videoReadableId]
 }))
 export default
-class VideoContent {
+class VideoContent extends Component {
   static propTypes = {
     videoData: PropTypes.object.isRequired,
     params: PropTypes.object.isRequired
@@ -30,14 +42,30 @@ class VideoContent {
     return maybeLoadVideo(store, nextParams.videoReadableId);
   }
 
+  constructor(props) {
+    super(props);
+    this.state = { showOverlay: true };
+  }
+
+  onFrameLoad() {
+    this.setState({ showOverlay: false });
+  }
+
   render() {
     const {videoData} = this.props;
 
     const youtubeId = videoData.youtubeId;
     const youtubeEmbedUrl = `https://www.youtube.com/embed/${youtubeId}`;
 
+    const overlayStyle = {
+      ...frameOverlayStyle,
+      backgroundImage: `url(https://i.ytimg.com/vi/${youtubeId}/hqdefault.jpg)`
+    };
+    const overlay = this.state.showOverlay ? <div style={overlayStyle} /> : "";
+
     return <div>
-      <iframe type="text/html" width="100%" height={192} src={youtubeEmbedUrl} style={{marginBottom: '8px'}} />
+      {overlay}
+      <iframe type="text/html" src={youtubeEmbedUrl} style={frameStyle} onLoad={this.onFrameLoad.bind(this)} />
       <div style={descriptionBoxStyle}>
         <p style={videoTitleStyle}>{videoData.title}</p>
         <p>{videoData.description}</p>
