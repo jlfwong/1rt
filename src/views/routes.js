f70ece23fe1ec61e770bcd9c80f18105d01b6590
Jamie Wong <jamie.lf.wong@gmail.com>
2015-07-30 17:57:35 -0700

    Expand route hierarchy

--- src/views/routes.js
+++ src/views/routes.js
@@ -6,10 +6,24 @@ import TutorialPage from 'views/TutorialPage';
 import NotFound from 'views/NotFound';
 import Redirect from 'views/Redirect';
 
+class Dummy {
+  render() {
+    return <div>
+      {this.props.children}
+    </div>;
+  }
+}
+
 export default (
   <Route component={App}>
-    <Route path=":domainSlug/:subjectSlug/:topicSlug/:tutorialSlug" component={TutorialPage}>
-      <Route path="v/:videoReadableId" component={VideoPage}/>
+    <Route path=":domainSlug" component={Dummy}>
+      <Route path=":subjectSlug" component={Dummy}>
+        <Route path=":topicSlug" component={Dummy}>
+          <Route path=":tutorialSlug" component={TutorialPage}>
+            <Route path="v/:videoReadableId" component={VideoPage}/>
+          </Route>
+        </Route>
+      </Route>
     </Route>
     <Route path="*" component={NotFound}/>
   </Route>
